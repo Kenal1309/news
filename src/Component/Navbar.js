2db066d6948a8a41d6d6de55f460934f21badb11
Kenal1309 <kenalp1329@gmail.com>
2023-09-11 17:25:32 +0530

    final

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -21,13 +21,8 @@ export class Navbar extends Component {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/Sports">
-                  Home
-                </Link>
-              </li>
+          <div className="collapse navbar-collapse " id="navbarSupportedContent" style={{marginLeft:'15%'}}>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
               <li className="nav-item">
                 <Link className="nav-link" to="/business">
                   Business
